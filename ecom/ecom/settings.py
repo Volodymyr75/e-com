6c4b547f30821d4e05a81f5355e149cdbccb068c
Volodymyr Strembovskyi <strembov@gmail.com>
2021-03-21 10:15:57 +0200

    add user views

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'api',
     'api.category',
-    'api.product',
+    'api.product'
 ]
 
 MIDDLEWARE = [
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AUTH_USER_MODEL = 'user.CustomUser'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
